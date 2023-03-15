d43b074a27d9fd3a134ec1246fa2bd18afd09cec
w-hutinglong <zbchtl@163.com>
2023-03-15 14:17:27 +0800

    提交

--- src/main/java/com/example/demo/helloworld.java
+++ src/main/java/com/example/demo/helloworld.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("测试Springboot工程");
+        System.out.println("测试Springboot工程,完成jenkins测试");
+        System.out.println("666666");
     }
 }
