508a9a38c168401e22166b18b11d11df5dd3fad3
w-hutinglong <zbchtl@163.com>
2023-03-17 15:56:45 +0800

    提交

--- src/main/java/com/example/demo/helloworld.java
+++ src/main/java/com/example/demo/helloworld.java
@@ -10,6 +10,6 @@ import javax.annotation.Resource;
 public class helloworld {
    @RequestMapping("/t1")
     public String test(){
-       return "ｈｔｌ测试jenkins成功";
+       return "ｈｔｌ测试jenkins成功本地";
    }
 }
